8d2a974fc73ddabab2e84e1ff2c87377fbb2597b
investlens <onetaskforindia@gmail.com>
2026-09-11 22:04:01 +0530

    feat: start production pipeline watchdog

--- src/startup.ts
+++ src/startup.ts
@@ -1,6 +1,7 @@
 import { startPumpPortalCreatorFeed } from './core/pumpPortalCreatorFeed.js';
 import { startRuntimeHealthHeartbeat } from './services/runtimeHealthHeartbeat.js';
 import { startShadowDecisionOutcomeGrader } from './intelligence/shadowDecisionOutcomeGrader.js';
+import { startSystemWatchdog } from './services/systemWatchdog.js';
 
 void startPumpPortalCreatorFeed().catch((error) => {
   console.log('[PumpPortalCreatorFeed] startup error', {
@@ -10,5 +11,6 @@ void startPumpPortalCreatorFeed().catch((error) => {
 
 startRuntimeHealthHeartbeat();
 startShadowDecisionOutcomeGrader();
+startSystemWatchdog();
 
 await import('./main.js');
