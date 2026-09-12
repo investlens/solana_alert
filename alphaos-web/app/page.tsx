0959f00361694f2898de00cc533abb94a99d33ea
investlens <onetaskforindia@gmail.com>
2026-09-12 10:55:04 +0530

    feat: surface verified live intelligence on home

--- alphaos-web/app/page.tsx
+++ alphaos-web/app/page.tsx
@@ -1,11 +1,13 @@
 import AppShell from "@/components/layout/AppShell";
 import AlphaHome from "@/components/home/AlphaHome";
+import LiveIntelligenceSummary from "@/components/home/LiveIntelligenceSummary";
 
 export const dynamic = "force-dynamic";
 
 export default function HomePage() {
   return (
     <AppShell>
+      <LiveIntelligenceSummary />
       <AlphaHome />
     </AppShell>
   );
